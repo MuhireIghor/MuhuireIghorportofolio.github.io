a1ee1a6d216bf0084529fb47a961a41592e06f39
MuhireIghor <muhireighor123@gmail.com>
2022-10-29 10:39:47 +0200

    security issue fixings

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,16 +1,35 @@
 require("dotenv").config();
 export default function(req,res){
     let nodemailer = require("nodemailer");
+    const password = process.env.PASSWORD;
+    const email = process.env.EMAIL;
+
     const transporter = nodemailer.createTransport(
         {
             port:465,
             host:"smtp.gmail.com",
             auth:{
-                user:"muhireighor123@gmail.com",
-                pass:"Frater Dei1"
+                user:email,
+                pass:password
             },
             secure:true
         }
     )
+    const mailData = {
+        from:req.body.email,
+        to:email,
+        subject:`Message from ${req.body.email}`,
+        text:req.body.message,
+        html:<div>{req.body.message}</div>
+
+    }
+    transporter.sendMail(mailData,(err,info)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log(info);
+        }
+    })
+    res.status(200);
     console.log(req.body);
 }
